5d04ca32b2715701af0d1004afdf7badaef92cbc
Marko Mikulicic <mmikulicic@gmail.com>
2022-09-15 15:57:58 +0200

    More tests

--- mirori_test.go
+++ mirori_test.go
@@ -32,41 +32,68 @@ func TestReadAll(t *testing.T) {
 
 func TestInterleaved(t *testing.T) {
 	in := "foobarbaz"
-	left, right := miroir.NewMiroir(strings.NewReader(in))
-	_ = right
-	testCases := []struct {
-		r    io.Reader
+
+	var right, left io.Reader
+
+	testCases := [][]struct {
+		r    *io.Reader
 		ln   int
 		want string
 		last bool
 	}{
-		{left, 3, "foo", false},
-		{right, 3, "foo", false},
-		{left, 3, "bar", false},
-		{left, 3, "baz", false},
-		{left, 3, "", true},
-		{right, 3, "bar", false},
-		{right, 4, "baz", true},
-		{left, 3, "", true},
+		{
+			{&left, 3, "foo", false},
+			{&right, 3, "foo", false},
+			{&left, 3, "bar", false},
+			{&left, 3, "baz", false},
+			{&left, 3, "", true},
+			{&right, 3, "bar", false},
+			{&right, 3, "baz", false},
+			{&right, 3, "", true},
+		},
+		{
+			{&left, 3, "foo", false},
+			{&right, 3, "foo", false},
+			{&left, 3, "bar", false},
+			{&left, 3, "baz", false},
+			{&left, 3, "", true},
+			{&right, 3, "bar", false},
+			{&right, 4, "baz", true},
+		},
+		{
+			{&left, 3, "foo", false},
+			{&left, 3, "bar", false},
+			{&left, 3, "baz", false},
+			{&left, 3, "", true},
+			{&right, 3, "foo", false},
+			{&right, 3, "bar", false},
+			{&right, 4, "baz", true},
+		},
+		{
+			{&left, 3, "foobar", false},
+		},
 	}
 
-	for i, tc := range testCases {
-		t.Run(fmt.Sprint(i), func(t *testing.T) {
-			buf := make([]byte, tc.ln)
-			n, err := tc.r.Read(buf)
-			eof := err == io.EOF
-			if err != nil && !eof {
-				t.Fatal(err)
-			}
-			if got, want := eof, tc.last; got != want {
-				t.Fatalf("got: %v, want: %v", got, want)
-			}
-			if got, want := n, len(tc.want); got != want {
-				t.Fatalf("got: %d, want: %d", got, want)
-			}
-			if got, want := string(buf[:n]), tc.want; got != want {
-				t.Fatalf("got: %q, want: %q", got, want)
-			}
-		})
+	for i, run := range testCases {
+		left, right = miroir.NewMiroir(strings.NewReader(in))
+		for j, tc := range run {
+			t.Run(fmt.Sprint(i, j), func(t *testing.T) {
+				buf := make([]byte, tc.ln)
+				n, err := (*tc.r).Read(buf)
+				eof := err == io.EOF
+				if err != nil && !eof {
+					t.Fatal(err)
+				}
+				if got, want := eof, tc.last; got != want {
+					t.Fatalf("got: %v, want: %v", got, want)
+				}
+				if got, want := n, len(tc.want); got != want {
+					t.Fatalf("got: %d, want: %d", got, want)
+				}
+				if got, want := string(buf[:n]), tc.want; got != want {
+					t.Fatalf("got: %q, want: %q", got, want)
+				}
+			})
+		}
 	}
 }
